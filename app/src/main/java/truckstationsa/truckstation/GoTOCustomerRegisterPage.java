655707ce121f359d0734ff8cd5babd6073901127
manal <swe.manalh@gmail.com>
2018-02-09 20:42:51 +0300

    غيرت البرالبرايميري كيز خليتها كلها سترنقز بعد ما شفت الفديو الي ارسلته عهود

--- app/src/main/java/truckstationsa/truckstation/GoTOCustomerRegisterPage.java
+++ app/src/main/java/truckstationsa/truckstation/GoTOCustomerRegisterPage.java
@@ -1,19 +1,64 @@
 package truckstationsa.truckstation;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+//import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+//import com.google.android.gms.location.places.Place;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 
 /**
  * Created by manal on 2/9/2018.
  */
 
 public class GoTOCustomerRegisterPage  extends AppCompatActivity {
-
+    String address;
+    Context context;
+    int PLACE_PICKER_REQUEST = 1;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.go_tO_customer_register_page);
-    }
-}
+        setContentView(R.layout.go_to_customer_register_page);
+        context = this;
+      //  location.setOnClickListener(new View.OnClickListener() {
+         //   @Override
+           // public void onClick(View v) {
+
+             /*   PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                // finish();
+                Intent intent;
+                try {
+                    address = "  ";
+                    intent = builder.build((Activity)  context);
+                    startActivityForResult(intent,PLACE_PICKER_REQUEST);
+
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });*/ }
+
+
+       }
